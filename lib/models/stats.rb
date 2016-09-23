ac5a108d84b6f98c5ecbb54c5034d3917232817a
Garrow Bedrossian <garrow.bedrossian@gmail.com>
2016-09-23 16:27:00 +1000

    Use crappier data for demo

--- lib/models/stats.rb
+++ lib/models/stats.rb
@@ -5,8 +5,8 @@ class Stats < Sequel::Model
 
 
   def self.history(repo)
-    DB["select id, calculated->'open' as open_prs, calculated->'needy' as needy from stats where repo_id = #{repo.id}"]
-    # DB["select id, calculated->'open' as open_prs, calculated->'needy' as needy from stats"]
+    # DB["select id, calculated->'open' as open_prs, calculated->'needy' as needy from stats where repo_id = #{repo.id}"]
+    DB["select id, calculated->'open' as open_prs, calculated->'needy' as needy from stats"]
   end
 end
 
